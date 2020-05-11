ca190e48cc19e381679f52960c988580919fc322
victorMenegass0 <victormenegasso013@gmail.com>
2020-05-12 06:39:31 +0900

    parte 1 da geolocalizacao

--- www/components/plug-in.js
+++ www/components/plug-in.js
@@ -47,6 +47,25 @@ $(document).on('click', '#vibrate', function(){
   navigator.vibrate(450);
 });
 
+//geolocation
+$(document).on('click','#local', function(){
+  var onSuccess = function(position) {
+        alert('Latitude: '          + position.coords.latitude          + '\n' +
+              'Longitude: '         + position.coords.longitude         + '\n' +
+              'Altitude: '          + position.coords.altitude          + '\n' +
+              'Accuracy: '          + position.coords.accuracy          + '\n' +
+              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+              'Heading: '           + position.coords.heading           + '\n' +
+              'Speed: '             + position.coords.speed             + '\n' +
+              'Timestamp: '         + position.timestamp                + '\n');
+  }
+  function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+  }
+  navigator.geolocation.getCurrentPosition(onSuccess, onError);
+});
+
 
 
 
