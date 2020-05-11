3252de52c2ae56762d0ca80e4bf9c124932e5a02
victorMenegass0 <victormenegasso013@gmail.com>
2020-05-12 06:06:54 +0900

    inserção dos plugins de notificaçoes

--- www/components/plug-in.js
+++ www/components/plug-in.js
@@ -18,3 +18,35 @@ $(document).on('click', '#camera', function(){
       alert('Failed because: ' + message);
   }
 })
+
+//notification e vibrate
+$(document).on('click', '#alert', function(){
+  navigator.notification.alert('you are the weiner', null, 'voce ganhou alguma coise ai', 'ok');
+});
+
+
+$(document).on('click', '#confirm', function(){
+  function retorno(){
+    if (retorno==1){
+      navigator.notification.alert('confirm text', null, 'tu aperto b', 'ok');
+    }
+    else if (retorno==2){
+      navigator.notification.alert('confirm text', null, 'tu aperto a', 'ok');
+    }
+  }
+  navigator.notification.confirm('mensagem', retorno, 'titulo', ['b','a']);
+});
+
+
+$(document).on('click', '#beep', function(){
+  navigator.notification.beep(3);
+});
+
+
+$(document).on('click', '#vibrate', function(){
+  navigator.vibrate(450);
+});
+
+
+
+
